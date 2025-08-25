94cdf18cc8920d431482f9ad1b059e67a3cff64f
Bosseyang <bosse_yang@hotmail.com>
2025-08-25 17:09:02 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
 import "../css/index.css";
 import { Header } from "./Header";
+import { TodoList } from "./TodoList";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <TodoList />
+      </main>
     </>
   );
 }
