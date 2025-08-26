ec4d14a794ea244279e7e5faa2ca65cde0f01bc2
Bosseyang <bosse_yang@hotmail.com>
2025-08-26 17:00:41 +0200

    Update AddTodo.tsx

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -26,15 +26,10 @@ export const AddTodo: React.FC<{ onAdd: (todo: ITodo) => void }> = ({
   return (
     <section className="add-todo">
       <form onSubmit={handleSubmit} className="add-todo-form">
-        <label htmlFor="text-input" className="input">
-          Todo
-        </label>
-        <input type="text" className="text-input" />
-        <label htmlFor="author-input" className="input">
-          Author
-        </label>
-        <input type="text" className="author-input" />
-        <button className="add-todo-btn" type="submit">
+        {/* <input type="text" className="text-input" placeholder="Todo" /> */}
+        <textarea className="text-input" placeholder="Todo" />
+        <input type="text" className="author-input" placeholder="Author" />
+        <button className="button add-todo-btn" type="submit">
           Add Todo
         </button>
       </form>
