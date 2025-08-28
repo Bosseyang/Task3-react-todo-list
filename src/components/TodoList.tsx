383577db9d039841c68bfea1f6b2e45e340923ac
Bosseyang <bosse_yang@hotmail.com>
2025-08-28 11:17:07 +0200

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,3 @@
-import { useState, type ReactElement } from "react";
-
-import type { ITodo } from "../types";
 import React from "react";
 import { AddTodo } from "./AddTodo";
 import { TodoItem } from "./TodoItem";
@@ -18,16 +15,10 @@ export const TodoList: React.FC = () => {
     setSortBy,
   } = useTodos();
 
-export const TodoList = (): ReactElement => {
-  const [todos, setTodos] = useState<ITodo[]>([]);
-  const addTodo = (todo: ITodo) => setTodos([...todos, todo]);
   return (
     <section className="todo-list">
       <AddTodo onAdd={addTodo} />
-      <h2>Todos</h2>
-      <ul>
         {todos.map((todo) => (
-          <TodoItem todo={todo} />
         ))}
       </ul>
     </section>
