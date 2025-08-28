6c17cb66a62ddf8e5739b97fefe4582fe30043bf
Bosseyang <bosse_yang@hotmail.com>
2025-08-28 17:03:01 +0200

    Update TodoItem.tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -28,14 +28,9 @@ export const TodoItem: React.FC<ITodoItemProps> = ({
   return (
     <li className="todo-item">
       <div className="todo-wrapper">
-        <button
-          className="button todo-completed-button"
-          onClick={() => onToggle(todo.id)}
-        >
+        <button className="button todo-checkbox-button" onClick={() => onToggle(todo.id)}>
           {todo.completed ? (
-            <span className="check-box material-symbols-outlined">
-              check_box
-            </span>
+            <span className="check-box material-symbols-outlined">check_box</span>
           ) : (
             <span className="check-box-blank material-symbols-outlined">
               check_box_outline_blank
@@ -50,40 +45,25 @@ export const TodoItem: React.FC<ITodoItemProps> = ({
               onChange={(e) => setEditText(e.target.value)}
             />
           ) : (
-            <span className={todo.completed ? "line-through" : ""}>
-              {todo.text}
-            </span>
+            <span className={todo.completed ? "line-through" : ""}>{todo.text}</span>
           )}
-          <div className="author-time-text">
-            By {todo.author} | {todo.timestamp.toLocaleString()}
-          </div>
+          <p className="author-time-text">
+            By <i>{todo.author}</i> | {todo.date.toLocaleString()}
+          </p>
         </div>
       </div>
 
       <div className="todo-controls-wrapper">
         <button className="button edit-button" onClick={handleEdit}>
-          {isEditing ? (
-            "Save"
-          ) : (
-            <span className="material-symbols-outlined">edit</span>
-          )}
+          {isEditing ? "Save" : <span className="material-symbols-outlined">edit</span>}
         </button>
-        <button
-          className="button remove-button"
-          onClick={() => onRemove(todo.id)}
-        >
+        <button className="button remove-button" onClick={() => onRemove(todo.id)}>
           <span className="material-symbols-outlined">delete</span>
         </button>
-        <button
-          className="button arrow-up-button"
-          onClick={() => onMove(todo.id, "up")}
-        >
+        <button className="button arrow-up-button" onClick={() => onMove(todo.id, "up")}>
           <span className="material-symbols-outlined">arrow_upward</span>
         </button>
-        <button
-          className="button arrow-down-button"
-          onClick={() => onMove(todo.id, "down")}
-        >
+        <button className="button arrow-down-button" onClick={() => onMove(todo.id, "down")}>
           <span className="material-symbols-outlined">arrow_downward</span>
         </button>
       </div>
