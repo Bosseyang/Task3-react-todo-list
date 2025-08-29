f6c37623a3400b697096636cf20f296ae53a450c
Bosseyang <bosse_yang@hotmail.com>
2025-08-29 09:58:49 +0200

    Update AddTodo.tsx

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -30,19 +30,21 @@ export const AddTodo: React.FC<IAddTodoProps> = ({ onAdd }) => {
     <section className="add-todo">
       <form onSubmit={handleSubmit} className="add-todo-form">
         {/* <input type="text" className="text-input" placeholder="Todo" /> */}
-        <textarea
-          className="text-input"
-          placeholder="Todo"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input
-          type="text"
-          className="author-input"
-          placeholder="Author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
+        <div className="add-todo-wrapper">
+          <textarea
+            className="text-input"
+            placeholder="Todo"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="text"
+            className="author-input"
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
         <button className="button add-todo-button" type="submit">
           Add Todo
         </button>
